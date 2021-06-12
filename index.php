f4a15e300db21baac62d6d68952ea0a71104f95f
Emil Alexe <emil.alexe92@gmail.com>
2021-06-12 14:36:34 +0300

    First commmit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NQ&#178;</title>
+        <meta charset="utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+        <style>
+            body{
+                width: 100%;
+                height: 100vh;
+                margin: 0px;
+                padding: unset;
+                background-image: url("/nq2-logo.svg");
+                background-size: 50%;
+                background-repeat: no-repeat;
+                background-position: center;
+            }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>
